fe8dad0dab526f809c35bb5ef75185d171f48c69
amirmorcos <amirhmorcos@gmail.com>
2024-04-04 14:07:29 +0200

    fix: project

--- android/app/src/main/java/com/app/MainApplication.kt
+++ android/app/src/main/java/com/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.socialapp
+package com.app
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/AppTests/AppTests.m
+++ ios/AppTests/AppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SocialAppTests : XCTestCase
+@interface AppTests : XCTestCase
 
 @end
 
-@implementation SocialAppTests
+@implementation AppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
