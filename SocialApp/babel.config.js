3369a9a034e1a88cec24fedf539d04ce9a80c7f7
amirmorcos <amirhmorcos@gmail.com>
2024-04-04 15:28:30 +0200

    add alias

--- SocialApp/babel.config.js
+++ SocialApp/babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          // This needs to be mirrored in tsconfig.json
+          atoms: './src/components/atoms',
+          molecules: './src/components/atoms',
+          organisms: './src/components/organisms',
+          screens: './src/screens',
+          navigation: './src/navigation',
+          redux: './src/redux',
+        },
+      },
+    ],
+  ],
 };
