9427bceabe2ecc01ec28b48edf11453268539f45
amirmorcos <amirhmorcos@gmail.com>
2024-04-08 10:08:41 +0200

    add: splash screen

--- SocialApp/App.tsx
+++ SocialApp/App.tsx
@@ -5,14 +5,18 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainStack from 'navigation/mainStack';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
 import {store} from 'store/index';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <Provider store={store}>
--- SocialApp/ios/SocialApp/AppDelegate.mm
+++ SocialApp/ios/SocialApp/AppDelegate.mm
@@ -1,15 +1,20 @@
 #import "AppDelegate.h"
 
+
 #import <React/RCTBundleURLProvider.h>
+#import "RNSplashScreen.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+ 
+  
   self.moduleName = @"SocialApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
